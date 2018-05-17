7805e6dc9ba63755ea3279789d0c579a31438c2c
oliverpople <oliverpople@gmail.com>
2018-05-17 11:39:53 -0700

    Modified Passport callback to check if user already exists before creating a new record.  Used Passport `done` function to let it know it can proceed with the authentication flow.

--- server/services/passport.js
+++ server/services/passport.js
@@ -13,7 +13,15 @@ passport.use(
 			callbackURL: "/auth/google/callback"
 		},
 		(accessToken, refreshToken, profile, done) => {
-			new User({ googleID: profile.id }).save();
+			User.findOne({ googleID: profile.id }).then(existingUser => {
+				if (existingUser) {
+					done(null, existingUser);
+				} else {
+					new User({ googleID: profile.id })
+						.save()
+						.then(user => done(null, user));
+				}
+			});
 		}
 	)
 );
