1d416b7e778b7b7629d7cde8490f946b57610ea8
admin <admin@admins-MacBook-Air.local>
2018-05-30 10:01:17 -0700

    Refactored passport function to include modern async and await syntax

--- services/passport.js
+++ services/passport.js
@@ -15,6 +15,23 @@ passport.deserializeUser((id, done) => {
 	});
 });
 
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const keys = require("../config/keys.js");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+	User.findById(id).then(user => {
+		done(null, user);
+	});
+});
+
 passport.use(
 	new GoogleStrategy(
 		{
@@ -23,16 +40,15 @@ passport.use(
 			callbackURL: "/auth/google/callback",
 			proxy: true
 		},
-		(accessToken, refreshToken, profile, done) => {
-			User.findOne({ googleID: profile.id }).then(existingUser => {
-				if (existingUser) {
-					done(null, existingUser);
-				} else {
-					new User({ googleID: profile.id })
-						.save()
-						.then(user => done(null, user));
-				}
-			});
+		async (accessToken, refreshToken, profile, done) => {
+			const existingUser = await User.findOne({ googleID: profile.id });
+
+			if (existingUser) {
+				done(null, existingUser);
+			} else {
+				const user = await new User({ googleID: profile.id }).save();
+				done(null, user);
+			}
 		}
 	)
 );
