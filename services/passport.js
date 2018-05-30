ff8728b7156742e86cab5672e0cc3227b906a8f5
oliverpople <oliverpople@gmail.com>
2018-05-30 10:45:07 -0700

    Refactored passport function further by replacing `else` with `return` in if statement.

--- services/passport.js
+++ services/passport.js
@@ -15,23 +15,6 @@ passport.deserializeUser((id, done) => {
 	});
 });
 
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const mongoose = require("mongoose");
-const keys = require("../config/keys.js");
-
-const User = mongoose.model("users");
-
-passport.serializeUser((user, done) => {
-	done(null, user.id);
-});
-
-passport.deserializeUser((id, done) => {
-	User.findById(id).then(user => {
-		done(null, user);
-	});
-});
-
 passport.use(
 	new GoogleStrategy(
 		{
@@ -44,11 +27,10 @@ passport.use(
 			const existingUser = await User.findOne({ googleID: profile.id });
 
 			if (existingUser) {
-				done(null, existingUser);
-			} else {
-				const user = await new User({ googleID: profile.id }).save();
-				done(null, user);
+				return done(null, existingUser);
 			}
+			const user = await new User({ googleID: profile.id }).save();
+			done(null, user);
 		}
 	)
 );
