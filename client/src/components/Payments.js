04b00cb18d8cb3fa50ed63be8147473366865c72
oliverpople <oliverpople@gmail.com>
2018-10-13 14:59:20 -0700

    Amending typo on Payments component Stripe checkout property,

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -2,15 +2,15 @@ import React, { Component } from "react";
 import StripeCheckout from "react-stripe-checkout";
 
 class Payments extends Component {
-	render() {
-		return (
-			<StripeCheckout
-				amoutn={500}
-				token={token => console.log(token)}
-				stripeKey={process.env.REACT_APP_STRIPE_KEY}
-			/>
-		);
-	}
+  render() {
+    return (
+      <StripeCheckout
+        amount={500}
+        token={token => console.log(token)}
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}
+      />
+    );
+  }
 }
 
 export default Payments;
