5636f01aa9436922cb8bd5132e77635729fec4f2
oliverpople <oliverpople@gmail.com>
2018-10-16 09:56:19 -0700

    Installed `stripe` npm module in order to create a charge object (for charging a card) via the stripe api.  Required Stripe library in billingRoutes file using the previously created the secretStripeKey as an argument. Amended typo.

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,5 +1,6 @@
-module.exports = app => {
-    app.post('/api/stripe', (res, res) => {
+const keys = require("../config/keys");
+const stripe = require("stripe")(keys.stripeSecretKey);
 
-    });
+module.exports = app => {
+  app.post("/api/stripe", (req, res) => {});
 };
