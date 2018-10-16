5fb203c4560613182486c8f0a41225454e0883a8
oliverpople <oliverpople@gmail.com>
2018-10-16 10:58:39 -0700

    Specified stripe API post request; amount, currency, description and source (the Stripe id property ie req.body.id) Amended typo.

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require("./routes/authRoutes")(app);
-require(",/routes/billingRoutes")(app);
+require("./routes/billingRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
