b12630441d7d53bdaa6625d91457ddade5f88836
oliverpople <oliverpople@gmail.com>
2018-10-17 10:32:25 -0700

    Configured Express for production mode. Now routes that aren1t recognized by the express server are checked to see whether the requested assets exist within the client/build folder.  If not, then Express will return the HTML file assuming the requested route is react-route that can be handled in the client side of the app.

--- index.js
+++ index.js
@@ -25,5 +25,13 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/buld"));
+  const path = require("path");
+  app.get("*", (rew, res) => {
+    res.sendFile(path.resolve(_dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
