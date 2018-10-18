68176b5ef52c4a6cff98a487b383b1b76a3ddc31
oliverpople <oliverpople@gmail.com>
2018-10-18 11:51:25 -0700

    Amending typo.

--- index.js
+++ index.js
@@ -26,10 +26,10 @@ require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/buld"));
   const path = require("path");
+  app.use(express.static(path.join(__dirname, "/client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(_dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
