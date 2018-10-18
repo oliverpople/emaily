a64e12caad870c44d5fcade8079c5bfa55c4ab96
oliverpople <oliverpople@gmail.com>
2018-10-18 15:42:22 -0700

    Attempting alternative syntax

--- index.js
+++ index.js
@@ -32,5 +32,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT);
+app.listen(process.env.PORT || 5000);
