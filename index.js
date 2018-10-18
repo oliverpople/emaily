f9e29c68180a10ee1ef2c1745cff819c9507682e
oliverpople <oliverpople@gmail.com>
2018-10-18 11:42:11 -0700

    Changed order of operations and tweak code in server production config in attmept to fix Heroku error.  Followed this suggestion https://www.udemy.com/node-with-react-fullstack-web-development/learn/v4/questions/37045.

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/buld"));
   const path = require("path");
+  app.use(express.static(path.join(_dirname, "client/buld")));
   app.get("*", (rew, res) => {
     res.sendFile(path.resolve(_dirname, "client", "build", "index.html"));
   });
