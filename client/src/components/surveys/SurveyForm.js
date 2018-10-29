0b68ad056b0aa2c109e4ac0a96d2df17bf55db5d
oliverpople <oliverpople@gmail.com>
2018-10-29 16:45:59 -0700

    Added Field component redux-form helper to SurveyForm component and added it to render function.

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,10 +1,14 @@
 // SurveyForm shows a form for a user to add input
 import React, { Component } from "react";
-import { reduxForm } from "redux-form";
+import { reduxForm, Field } from "redux-form";
 
 class SurveyForm extends Component {
   render() {
-    return <div>SurveyForm!</div>;
+    return (
+      <div>
+        <Field type="text" name="surveyTitle" component="input" />
+      </div>
+    );
   }
 }
 
