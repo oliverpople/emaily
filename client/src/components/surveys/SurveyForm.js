42f1af927ca8690cf894c5bc08d5df241c3e78a3
oliverpople <oliverpople@gmail.com>
2018-10-29 16:09:08 -0700

    Created surveyForm component and imported it into surveyNew, adding it to the render method.

---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,10 @@
+// SurveyForm shows a form for a user to add input
+import React, { Component } from "react";
+
+class SurveyForm extends Component {
+  render() {
+    return <div>SurveyForm!</div>;
+  }
+}
+
+export default SurveyForm;
