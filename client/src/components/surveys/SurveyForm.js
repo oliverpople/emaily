7855264b1e5acdc589de394e06ee7ecbd3b57af4
oliverpople <oliverpople@gmail.com>
2018-10-29 16:31:56 -0700

    Connected SurveyForm component to redux store using the reduxForm helper function.

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,5 +1,6 @@
 // SurveyForm shows a form for a user to add input
 import React, { Component } from "react";
+import { reduxForm } from "redux-form";
 
 class SurveyForm extends Component {
   render() {
@@ -7,4 +8,6 @@ class SurveyForm extends Component {
   }
 }
 
-export default SurveyForm;
+export default reduxForm({
+  form: "surveyForm"
+})(SurveyForm);
