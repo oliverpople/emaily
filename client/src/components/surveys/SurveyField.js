f14f7b8e8abe4012a8409640cdc46824dd74d36f
oliverpople <oliverpople@gmail.com>
2018-10-29 17:17:28 -0700

    Created SurveyField component and a function that uses redux Field to render it inside the SurveyFrom component.

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,10 @@
+// SurveyField contains logic to render a single label and text inpout
+import React from "react";
+
+export default () => {
+  retrun(
+    <div>
+      <input />
+    </div>
+  );
+};
