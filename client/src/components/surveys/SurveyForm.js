af87519eb50307b43a4499796238a3d943f50f97
oliverpople <oliverpople@gmail.com>
2018-10-30 10:45:31 -0700

    Added Cancel button to SurveyForm which uses the react-router-dom `Link` feature to link the user back to the /surveys route onClick.

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -2,6 +2,7 @@
 import _ from "lodash";
 import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
+import { Link } from "react-router-dom";
 import SurveyField from "./SurveyField";
 
 const FIELDS = [
@@ -30,6 +31,9 @@ class SurveyForm extends Component {
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
           {this.renderFields()}
+          <Link to="/surveys" className="red btn-flat white-text">
+            Cancel
+          </Link>
           <button type="submit" className="teal btn-flat right white-text">
             Next
             <i className="material-icons right">done</i>
