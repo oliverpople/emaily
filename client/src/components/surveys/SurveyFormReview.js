6f93bf826c1e768345e1f9f562a994ec2d441a8d
oliverpople <oliverpople@gmail.com>
2018-11-01 09:11:23 -0700

    Reduced tight coupling with FIELDS object in SurveyForm component by extracting the FIELDS object into separate file and importing it into SurveyForm and SurveyFormReview components.  Used the FIELDS object and lodash to refactor JSX duplication  in SurveyFormReview component; dynamically generating reviewFields.  Added key identifier to reviewFields as they generate a list.

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,25 +1,23 @@
 // SurveyFormReview shows users their form inputs for review
+import _ from "lodash";
 import React from "react";
 import { connect } from "react-redux";
+import formFields from "./formFields";
 
 const SurveyFormReview = ({ onCancel, formValues }) => {
+  const reviewFields = _.map(formFields, field => {
+    return (
+      <div key={field.name}>
+        <label>{field.label}</label>
+        <div>{formValues[field.name]}</div>
+      </div>
+    );
+  });
+
   return (
     <div>
       <h5>Please confirm your entries</h5>
-      <div>
-        <div>
-          <label>Survey Title</label>
-          <div>{formValues.title}</div>
-        </div>
-        <div>
-          <label>Subject Line</label>
-          <div>{formValues.subject}</div>
-        </div>
-        <div>
-          <label>Email Body</label>
-          <div>{formValues.body}</div>
-        </div>
-      </div>
+      {reviewFields}
       <button className="yellow darken-3 btn-flat" onClick={onCancel}>
         Back
       </button>
